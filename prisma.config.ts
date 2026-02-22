8f284a705ba76de0ef08f5e2ffbea5721a20fce5
evelotion <indradwigaming@gmail.com>
2026-02-22 15:32:44 +0700

    🚀 Database ready, config secured!

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from '@prisma/config';
+﻿import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
   datasource: {
-    url: process.env.DATABASE_URL,
+    // 👇 GANTI pakai URL Database Neon lo yang panjang itu
+    url: "postgresql://neondb_owner:npg_zbkpIqQG9oD6@ep-fragrant-pine-a1mrqcda-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   },
   migrations: {
-   
     seed: 'npx tsx --env-file=.env prisma/seed.ts',
   },
 });
